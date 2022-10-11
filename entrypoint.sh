41befd5de4fe91b8933d43be882ae576ee6ce162
ockwe <115560087+ockwe@users.noreply.github.com>
2022-10-11 06:35:02 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "273413b1-5890-43f9-bc41-57fde504555c"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
